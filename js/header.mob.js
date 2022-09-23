34db59a2fa203a026685cf8a956905e6d027428e
jkmiii <jkmiii89@naver.com>
2022-09-23 17:44:48 +0900

    mobile-header

---
+++ js/header.mob.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.drop-menu__title').click(function(){
+    $('.drop-menu__down').stop().slideUp();
+  });
+
+  $('.drop-menu__title').click(function(){
+    $('.drop-menu__down').stop().slideDown();
+  
+  });
+})
