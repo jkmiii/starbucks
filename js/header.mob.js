54661d232312443d1513508755686f6c4ebc8579
jkmiii <jkmiii89@naver.com>
2022-09-26 17:04:34 +0900

    header-mob__drop-menu

--- js/header.mob.js
+++ js/header.mob.js
@@ -1,10 +1,35 @@
-$(document).ready(function(){
-  $('.drop-menu__title').click(function(){
-    $('.drop-menu__down').stop().slideUp();
-  });
+$(document).ready(function () {
 
-  $('.drop-menu__title').click(function(){
-    $('.drop-menu__down').stop().slideDown();
-  
+
+    $('.drop-menu__title').click(function(){
+      $('.drop-menu__title').removeClass('on');
+      $(this).addClass('on');
+      $(this).children('.drop-menu__down').slideUp();
+      if($(this).children('.drop-menu__down').is(':hidden')){
+        $(this).children('.drop-menu__down').slideDown();
+      } else{
+        $(this).children('.drop-menu__down').slideUp();
+      }
+      return false;
+    });
+    
+    $('.drop-menu__txt').click(function(){
+      $(this).children('.drop-menu__down__down').slideUp();
+      if($(this).children('.drop-menu__down__down').is(':hidden')){
+        $(this).children('.drop-menu__down__down').slideDown();
+      } else{
+        $(this).children('.drop-menu__down__down').slideUp();
+      }
+      return false;
+    });
+
+    
+
+
+
+
+  /*   $('.drop-menu__title a').click(function(){
+      $(this).addClass('on');
+    }) */
+
   });
-})
