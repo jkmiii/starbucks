9a7df3760615d79f6b6e6a4ac96381de0afc1766
jkmiii <jkmiii89@naver.com>
2022-10-02 22:31:10 +0900

    folder structure repactoring

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
+$("#header").load("/layouts/header.html");
+$("#footer").load("/layouts/footer.html");
+$("#header-mob").load("/layouts/header-mob.html");
 
-
-  $('#header').load("/header.html");
-	$('#footer').load("/footer.html");
-	$('#header-mob').load("/header-mob.html");
-
-
-
-
-$(window).on("load",function(){
-
-  $('#main-menu>li').mouseover(function(){
-    $(this).find('#slider').stop().slideDown();
-  }).mouseout(function(){
-    $(this).find('#slider').stop().slideUp();
-  });
-
-})
+$(window).on("load", function () {
+  $("#main-menu>li")
+    .mouseover(function () {
+      $(this).find("#slider").stop().slideDown();
+    })
+    .mouseout(function () {
+      $(this).find("#slider").stop().slideUp();
+    });
+});
 
 //#coffee 메뉴끝//
 
-
-$('#next').on('click', function() {
-  $('.slide__banner').css('transform', 'translateX(-819px)');
+$("#next").on("click", function () {
+  $(".slide__banner").css("transform", "translateX(-819px)");
 });
 
-$('#prev').on('click', function() {
-  $('.slide__banner').css('transform', 'translateX(819px)');
+$("#prev").on("click", function () {
+  $(".slide__banner").css("transform", "translateX(819px)");
 });
-
-
-
 
 /* $(document).ready(function(){
   $('#prev').click(function(){
@@ -47,14 +38,12 @@ $('#prev').on('click', function() {
 })
  */
 
-
-
-$('.autoplay').slick({
+$(".autoplay").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   dots: true,
   speed: 1000,
-  arrows : true, 	
+  arrows: true,
 });
